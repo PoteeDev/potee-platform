88550330bc09fb5f0e9191f5e07ac7f8d5f79bc8
Ivan Khakhanov <ikhakhanov@ptsecurity.ru>
2022-08-02 18:59:56 +0300

    add multiple checkers

--- scripts/test.py
+++ scripts/test.py
@@ -31,11 +31,19 @@ def main():
     if sys.argv[1] == "ping":
         return randint(1, 1)
     elif sys.argv[1] == "put":
-        key = randint(0, 100000)
-        fake_storage[key] = sys.argv[3]
-        return key
+        if sys.argv[2] == "comment":
+            key = randint(0, 100000)
+            fake_storage[key] = sys.argv[4]
+            return key
+        if sys.argv[2] == "auth":
+            key = randint(0, 100000)
+            fake_storage[key] = sys.argv[4]
+            return key
     elif sys.argv[1] == "get":
-        return fake_storage.get(sys.argv[3])
+        if sys.argv[2] == "comment":
+            return fake_storage.get(sys.argv[4])
+        if sys.argv[2] == "auth":
+            return fake_storage.get(sys.argv[4])
     elif sys.argv[1] == "exploit":
         if sys.argv[2] == "sql":
             return randint(0, 1)
