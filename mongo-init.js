3a9250e06d0dada6c1d9b62208e9a7a6ab46820a
Ivan Khakhanov <ikhakhanov@ptsecurity.ru>
2022-08-03 19:52:01 +0300

    add dev file

---
+++ mongo-init.js
@@ -0,0 +1,9 @@
+db.entities.insert({ 
+    created_at : ISODate("2022-08-02T13:31:08.047Z"), 
+    updated_at : ISODate("2022-08-02T13:31:08.047Z"), 
+    name : "naliway", 
+    login : "naliway", 
+    hash : "$2a$10$GtU7F5PW.jT0VFocSiP8q.W8rZ2Rc0iPsrZSf5mZCOhQvR/hdRlAa", 
+    visible : true, 
+    blocked : false 
+})
