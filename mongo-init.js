fd4948901c701bd5d105d8757247a80d5c0ebd4b
Ivan Khakhanov <ikhakhanov@ptsecurity.ru>
2022-10-27 21:28:59 +0300

    add ip to test data

--- mongo-init.js
+++ mongo-init.js
@@ -6,6 +6,8 @@ db.entities.insert([{
     updated_at: ISODate("2022-08-02T13:31:08.047Z"),
     name: "naliway",
     login: "naliway",
+    subnet: "10.0.1.0/24",
+    ip: "10.0.1.10",
     hash: "$2a$10$11sq8vxrjIJcS4PkSupjQeseU1JJFXDUBFLi7ONPlHJox8wfSNGLC",
     visible: true,
     blocked: false
@@ -15,6 +17,8 @@ db.entities.insert([{
     updated_at: ISODate("2022-08-02T13:31:08.047Z"),
     name: "nakateam",
     login: "nakateam",
+    subnet: "10.0.2.0/24",
+    ip: "10.0.2.10",
     hash: "$2a$10$tHlqGIrVoJbcV6sgVfQ9cuxI7bhPw39Lwu9lqCkr7btG8NIGJLBcC",
     visible: true,
     blocked: false
