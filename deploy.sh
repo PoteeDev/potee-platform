fe1ff9386dfd146c39adf363f5b3d7752ac74f96
Ivan Khakhanov <ikhakhanov@ptsecurity.ru>
2022-12-09 22:19:54 +0300

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,10 @@ then
     ssh-keygen -t rsa -f ./$filename -b 4096 -q -P ""
     ansible-playbook --version
     terraform  init
-    terraform plan  -var-file config.tfvars.json
+    terraform plan -var "cloudflare_api_token=$CF_API_TOKEN" -var-file config.tfvars.json
 fi
 
-terraform $command -auto-approve -var-file config.tfvars.json
+terraform $command -auto-approve -var "cloudflare_api_token=$CF_API_TOKEN" -var-file config.tfvars.json
 
 if [ $? -eq 0 ] && [ $command == "apply" ];
 then
