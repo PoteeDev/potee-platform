30ef4c5b1bb8723c0416f93ce4ee826fa30a551b
Ivan Hahanov <ivanhahanov13@gmail.com>
2023-08-23 21:43:54 +0300

    test deploy task and update checkers

--- mongo-init.js
+++ mongo-init.js
@@ -7,7 +7,7 @@ db.entities.insert([{
     name: "naliway",
     login: "naliway",
     subnet: "10.0.1.0/24",
-    ip: "10.0.1.10",
+    ip: "localhost",
     hash: "$2a$10$11sq8vxrjIJcS4PkSupjQeseU1JJFXDUBFLi7ONPlHJox8wfSNGLC",
     visible: true,
     blocked: false
@@ -18,7 +18,7 @@ db.entities.insert([{
     name: "nakateam",
     login: "nakateam",
     subnet: "10.0.2.0/24",
-    ip: "10.0.2.10",
+    ip: "localhost",
     hash: "$2a$10$tHlqGIrVoJbcV6sgVfQ9cuxI7bhPw39Lwu9lqCkr7btG8NIGJLBcC",
     visible: true,
     blocked: false
